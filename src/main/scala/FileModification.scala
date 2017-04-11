a428aa0fa806e73e1d52cb9b51eb1cc9f16cad5f
shivangi <shivangi.gupta@knoldus.in>
2017-04-11 11:16:04 +0530

    PDF functionality WIP

--- src/main/scala/FileModification.scala
+++ src/main/scala/FileModification.scala
@@ -4,13 +4,6 @@ import com.typesafe.config.ConfigFactory
 import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConverters._
-import org.xhtmlrenderer.pdf.ITextRenderer
-//import com.lowagie.text.DocumentException
-
-import java.io.FileOutputStream
-
-
-
 
 class FileModification {
   val logger = LoggerFactory.getLogger(classOf[FileModification])
@@ -23,6 +16,7 @@ class FileModification {
   val headerContent: String = Source.fromFile(ConfigFactory.load().getString("headerPath")).mkString
   val footerContent: String = Source.fromFile(ConfigFactory.load().getString("footerPath")).mkString
   val location = ConfigFactory.load().getString("outputFileLocation")
+  val Mdlocation = ConfigFactory.load().getString("MdFileLocation")
   val fileReadObject = new MdFilehandler
 
   /**
@@ -38,7 +32,8 @@ class FileModification {
       getFileData match {
         case Some(data: String) => val fileData = fileReadObject.ConvertMdExtension(data)
           logger.info("Begin writing [" + file + "." + outputFileExtension + "] at " + location)
-          writeToFile(location + file + outputFileExtension, fileData)
+          writeHtmlFile(location + file + outputFileExtension, fileData)
+          writeMdFile(Mdlocation + file + inputFileExtension, fileURLContent)
           logger.info("Successfully written [" + file + "." + outputFileExtension + "] at " + location)
           "Success"
         case None => logger.error(s"$file Conversion failed ")
@@ -69,31 +64,37 @@ class FileModification {
     * @param path storage location of the file
     * @param data contents of the file
     */
-  def writeToFile(path: String, data: String): Unit = {
+  def writeHtmlFile(path: String, data: String): Unit = {
     val writer = new PrintWriter(new File(path))
     writer.write(headerContent + data + footerContent)
+    writer.close()
+  }
+
+  def writeMdFile(path: String, data: String): Unit = {
+    val writer = new PrintWriter(new File(path))
+    writer.write(data)
     writer.close()
   }
 
   /**
     * Converts generated html files to their pdf version
     */
-  def convertToPdf() ={
-    var fos: FileOutputStream = null
-    val File_To_Convert  ="src/main/webapp/installation-guide.html"
-    val url = new File(File_To_Convert)
-    logger.info("url : "+url)
-    val HTML_TO_PDF = "src/main/webapp/installation-guide.pdf"
-    logger.info("HTML_TO_PDF :"+HTML_TO_PDF)
-    fos = new FileOutputStream(HTML_TO_PDF)
-    logger.info("fos :"+fos)
-    val renderer = new ITextRenderer()
-    val data=Source.fromFile("src/main/webapp/demoFile.html").mkString
-    logger.info("Data from html: "+data)
-    renderer.setDocument(data)
-    renderer.layout()
-    renderer.createPDF(fos)
-    fos.close()
-  }
+  //  def convertToPdf() = {
+  //    var fos: FileOutputStream = null
+  //    val File_To_Convert = "src/main/webapp/installation-guide.html"
+  //    val url = new File(File_To_Convert)
+  //    logger.info("url : " + url)
+  //    val HTML_TO_PDF = "src/main/webapp/installation-guide.pdf"
+  //    logger.info("HTML_TO_PDF :" + HTML_TO_PDF)
+  //    fos = new FileOutputStream(HTML_TO_PDF)
+  //    logger.info("fos :" + fos)
+  //    val renderer = new ITextRenderer()
+  //    val data = Source.fromFile("src/main/webapp/demoFile.html").mkString
+  //    logger.info("Data from html: " + data)
+  //    renderer.setDocument(data)
+  //    renderer.layout()
+  //    renderer.createPDF(fos)
+  //    fos.close()
+  //  }
 }
 
